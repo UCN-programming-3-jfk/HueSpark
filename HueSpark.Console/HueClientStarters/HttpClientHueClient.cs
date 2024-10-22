a267ef952d0b0f9af6fb4289621ee2f388563c84
Jakob Farian Krarup <kr@rup.dk>
2024-10-22 18:11:04 +0200

    Done for the day

--- HueSpark.Console/HueClientStarters/HttpClientHueClient.cs
+++ HueSpark.Console/HueClientStarters/HttpClientHueClient.cs
@@ -1,10 +1,17 @@
 ﻿using HueSpark.Console.Model;
 using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
 using System.Text.Json;
 
 namespace HueSpark.Console.HueClientStarters;
 public class HttpClientHueClient
 {
+    public enum Color {Red, Yellow, Green }
+    private Dictionary<Color, double[]> _colorLookUp = new() 
+    {
+        {Color.Green, new double[]{0.3, 0.7} }
+    };
+
     private HttpClient _client;
 
     public HttpClientHueClient(string baseUrl)
@@ -12,34 +19,59 @@ public class HttpClientHueClient
         _client = new() { BaseAddress = new Uri(baseUrl) };
     }
 
+    // task 1 - C# function to turn a light on/off based on light number and boolean
     public async Task SetLightOnOffAsync(int lightId, bool lightOn)
     {
        var content = JsonContent.Create(new { on = lightOn});
-       var result = await _client.PutAsync("lights/1/state", content);
+       var result = await _client.PutAsync($"lights/{lightId}/state", content);
        System.Console.WriteLine(await result.Content.ReadAsStringAsync());
     }
+
+    //public async Task ToggleLight(int id) 
+    //{
+    //    var state = await GetLightStateAsync(id);
+    //    state.on = !state.on;
+    //    await SetLightStateAsync(id, state);
+    //}
+
+
+    public async Task ToggleLight(int id)
+    {
+        //hent lysets state
+        var lightState = await GetLightStateAsync(id);
+
+        //invertér state.on
+        lightState.on = !lightState.on;
+
+        //put lysets state
+        await SetLightStateAsync(id, lightState);
+    }
+
+    public async Task SetBulbColor(int id, double x, double y)
+    {
+        await SetLightStateAsync(id, new { xy = new[] {x,y} });
+    }
+    public async Task SetBulbColor(int id, Color color)
+    {
+        await SetLightStateAsync(id, new { xy = _colorLookUp[color] });
+    }
+
+
+
     public async Task SetLightStateAsync(int lightId, dynamic state)
     {
         var content = JsonContent.Create(state);
-        var result = await _client.PutAsync("lights/1/state", content);
+        var result = await _client.PutAsync($"lights/{lightId}/state", content);
         System.Console.WriteLine(await result.Content.ReadAsStringAsync());
-    }
+    }   
 
     public async Task<LightState> GetLightStateAsync(int lightId)
     {
-        try
-        {
-            var response = await _client.GetStringAsync("lights/1/");
+            var response = await _client.GetStringAsync($"lights/{lightId}/");
             using JsonDocument doc = JsonDocument.Parse(response);
             JsonElement stateElement = doc.RootElement.GetProperty("state");
 
             return JsonSerializer.Deserialize<LightState>(stateElement.GetRawText());
-        }
-        catch (Exception ex)
-        {
-            System.Console.WriteLine($"An error occurred: {ex.Message}");
-            return null;
-        }
     }
 
 }
--- HueSpark.Console/Program.cs
+++ HueSpark.Console/Program.cs
@@ -6,9 +6,20 @@ internal class Program
     public static async Task Main(string[] args)
     {
         HttpClientHueClient client = new("http://192.168.1.100/api/nOUSgAbLjknrHt-5eQmbRauCm3BuFoeJXaXQsdMt/");
-        await client.SetLightStateAsync(1, new{on=true, xy = new[] {0.8, 0.14 } });
+        await client.SetLightStateAsync(1, new { on = true, xy = new[] { 0.8, 0.14 } });
 
         var lightState = await client.GetLightStateAsync(1);
         System.Console.WriteLine($"Light is on: {lightState.on}");
+
+        //for (int i = 0; i < 20; i++)
+        //{
+        //    await client.ToggleLight(1);
+        //    await Task.Delay(1000);
+        //}
+        var lightId = 3;
+        await client.SetLightOnOffAsync(lightId, true);
+        //await client.SetBulbColor(lightId, 0.3, 0.7);
+        await client.SetBulbColor(lightId, HttpClientHueClient.Color.Green);
+
     }
 }
